eb993b3d56d666839218f1931b2583926dc68137
PeterJurkacek <peter.juek@gmail.com>
2017-08-20 17:40:13 +0200

    Add mapbox_feature

--- bumps-detector/ViewController.swift
+++ bumps-detector/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import Mapbox
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MGLMapViewDelegate{
+   
+    @IBOutlet weak var mapView: MGLMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        let point = MGLPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: 48.1585147, longitude: 17.0948126)
+        point.title = "Voodoo Doughnut"
+        point.subtitle = "22 SW 3rd Avenue Portland Oregon, U.S.A."
+
+        self.mapView.userTrackingMode = .follow
+        self.mapView.addAnnotation(point)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        // Always try to show a callout when an annotation is tapped.
+        return true
+    }
+    
+    @IBAction func createBump(_ sender: Any) {
+        //TODO: Získať údaj o aktuálnej polohe
+        
+        //TODO: Vytvoriť JSON object
+        //TODO: Poslať http post request na MySQL
+    }
 
 }
 
